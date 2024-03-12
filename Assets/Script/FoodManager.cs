83839ebd5233afe116d21fd2661928d698bab312
Eva291204 <eva.fricaud@e-artsup.net>
2024-03-12 10:43:47 +0100

    FoodManager

---
+++ Assets/Script/FoodManager.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class FoodManager : MonoBehaviour
+{
+    private static FoodManager _foodInstance;
+    public static FoodManager Instance
+    {
+        get
+        {
+            if (_foodInstance == null)
+            {
+                Debug.Log("FoodManager is null");
+            }
+            return _foodInstance;
+        }
+    }
+    public void Awake()
+    {
+        if (_foodInstance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _foodInstance = this;
+        }
+    }
+
+    public FoodStruct[] FoodList;
+}
+
+[Serializable]
+public struct FoodStruct
+{
+    public int NumberTag;
+    public GameObject Food;
+}
