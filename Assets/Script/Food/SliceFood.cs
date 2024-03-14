838a8e05fbccec1b7b5f6c32dfb89f2941d64126
Eva291204 <eva.fricaud@e-artsup.net>
2024-03-14 09:52:48 +0100

    cheese prefab

--- Assets/Script/Food/SliceFood.cs
+++ Assets/Script/Food/SliceFood.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 
 public class SliceFood : MonoBehaviour
 {
-    [SerializeField] public List<GameObject> _foodToSlice = new List<GameObject>(); 
+    [SerializeField] public List<GameObject> _foodToSlice = new List<GameObject>();
     [SerializeField] private List<GameObject> _foodSlice = new List<GameObject>();
     public bool CanSlice;
 
     public void OnCollisionEnter(Collision collision)
     {
+
         if (CanSlice)
         {
-            if (collision.gameObject.name == _foodToSlice[0].name) // pasteque
+            for (int i = 0; i < _foodSlice.Count; i++)
             {
-                Instantiate(_foodSlice[0]);
-                Instantiate(_foodSlice[0]);
-                _foodSlice[0].transform.position = collision.transform.position;
-                Destroy(collision.gameObject);
+                if (collision.gameObject.name == _foodToSlice[i].name)
+                {
+                    Instantiate(_foodSlice[i]);
+                    Instantiate(_foodSlice[i]);
+                    _foodSlice[i].transform.position = collision.transform.position;
+                    Destroy(collision.gameObject);
+                }
             }
-            else if (collision.gameObject.name == _foodToSlice[1].name) //cow
+            if (collision.gameObject.name == _foodToSlice[1].name) //cow
             {
                 Instantiate(_foodSlice[1]);
                 _foodSlice[1].transform.position = collision.transform.position;
